e56f1f68fbdc0e45a81d8604548e9e61b8d9516a
795717 <65739539+795717@users.noreply.github.com>
2020-05-24 17:36:14 -0700

    Ocean class created

---
+++ Ocean.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Ocean World
+ * 
+ * @author Christina Young, Jessica Yu, Annika Lin
+ * @version May 2020
+ */
+public class Ocean extends World
+{
+
+    /**
+     * Constructor for objects of class Ocean.
+     * 
+     */
+    public Ocean()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
