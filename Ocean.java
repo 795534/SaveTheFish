dc1754593c29795d6fafb4005a857140cb0676f9
jessicayu62 <41123985+jessicayu62@users.noreply.github.com>
2020-05-28 20:47:38 -0700

    Update to pass/fail

--- Ocean.java
+++ Ocean.java
@@ -11,9 +11,8 @@ public class Ocean extends World
     private GreenfootSound ocean = new GreenfootSound("OceanSounds.mp3");  
     private GreenfootSound music = new GreenfootSound("OceanMusic.mp3");
     private int timer = 0;
-    static ArrayList<Trash> trashItems = new ArrayList<Trash>();
+    static ArrayList<Trash> trashItems;
     static int numAlive;
-    static int numTrash;
     
     /**
      * Constructor for objects of class Ocean.
@@ -24,6 +23,8 @@ public class Ocean extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);   
         setBackground("intro.png");
+        trashItems = new ArrayList<Trash>();
+        numAlive = 0;
         //Intro in = new Intro();
         //addObject(in, 300,200);
         //removeObject(in);
@@ -91,7 +92,7 @@ public class Ocean extends World
             
             Greenfoot.stop();
         }
-        if(numTrash == 0)
+        if(trashItems.size() == 0)
         {
             Pass pass= new Pass();
             addObject(pass, 300, 200);
@@ -109,12 +110,8 @@ public class Ocean extends World
         int randomY = Greenfoot.getRandomNumber(80);
         Trash newTrash = new Trash(randomX, randomY);
         addObject(newTrash, randomX, randomY);
-        numTrash++;
         trashItems.add(newTrash);
         Greenfoot.playSound("BubblesShort.mp3");
     }
-    public int trash()
-    {
-        return numTrash;
-    }    
+ 
 }
